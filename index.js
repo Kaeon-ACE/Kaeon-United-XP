d96853e2303469f9b7b5e6065cecb471ac234e24
Jesse Dinkin <kaeon.ace@gmail.com>
2024-06-01 08:52:56 -0400

    Kaeon

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require("http").createServer((request, response) => {
- 
-	if(request.url == "/favicon.ico")
-		return;
- 
-	response.setHeader("Content-Type", "text/plain");
-	response.write(`THE URL IS: ${request.url}`);
-
-	response.end();
-}).listen(80);
+require("kaeon-united")("openAxis").openAxis({
+	routes: {
+		api: { "/api/": process.cwd() + "/api" },
+		default: {
+			index: "https://kaeon.gitlab.io/kaeon-united/"
+		},
+		public: { "/": process.cwd() + "/public" }
+	}
+});
